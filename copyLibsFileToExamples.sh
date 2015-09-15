c80d07752da6b6981cff0e9afaebbd419d7636ed
cwjpalmer <cwjpalmer@gmail.com>
2015-09-15 14:30:46 +0200

    scripts to update secondary libraries file on all example sketches, and to distribute library locally for dev work

---
+++ copyLibsFileToExamples.sh
@@ -0,0 +1,1 @@
+echo ~/Dropbox/WC/Awesome-Shield-Library/examples/*/*/ | xargs -n 1 cp ~/Dropbox/WC/Awesome-Shield-Library/examples/new-sketch/libraries.ino
---
+++ distribute.sh
@@ -0,0 +1,2 @@
+sh copyLibsFileToExamples.sh
+sh arduino-ide.sh
